93f6f45a39afe9c37be92810af72f1794565e8ab
Jonathan <jonathan.trhodes07@gmail.com>
2021-07-16 16:27:21 -0500

    working on arrays

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,9 @@
-var number = prompt("Choose and odd number between 1-50")
+do {
+    var number = prompt("Choose and odd number between 1-50")
+    if (number % 2 === 0) {
+        alert('That number is invalid')
+        break;
+    }
+
+    console.log(number)
+}while (number > 0)
--- js/for_loops.js
+++ js/for_loops.js
@@ -3,17 +3,18 @@
 //         console.log(i + " x " + input + " = " + (i * input));
 // }
 for (var count = 1; count <= 10; ++count) {
-        var numb = Math.floor(Math.random() * 180) + 20
-        if (numb % 2 === 0) {
-            console.log(numb + " is even")
-        } else {
-            console.log(numb + " is odd")
-        }
-//     }
-for (var i = 1; i <= 9; ++i){
-    var length = i.toString().repeat(i)
-    console.log(length)
-}
-for (var i = 100; i > 0; i = i - 5){
-    console.log(i)
-}
+    var numb = Math.floor(Math.random() * 18112                                                                                                                                                                                                                                                                                                                                                                             ) + 20
+    if (numb % 2 === 0) {
+        console.log(numb + " is even")
+    } else {
+        console.log(numb + " is odd")
+    }
+     }
+    for (var i = 1; i <= 9; ++i) {
+        var length = i.toString().repeat(i)
+        console.log(length)
+    }
+    for (var i = 100; i > 0; i = i - 5) {
+        console.log(i)
+    }
+
