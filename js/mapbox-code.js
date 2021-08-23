84070953a7f328fea5730ab7a477d41c703022a5
Jonathan <jonathan.trhodes07@gmail.com>
2021-08-23 16:01:19 -0500

    got the weather map finished just need styling

--- js/mapbox-code.js
+++ js/mapbox-code.js
@@ -20,12 +20,43 @@ function reverseGeocode(coordinates, token) {
             return data.features[0].place_name;
         });
 }
-    var accessToken = MAPBOX_TOKEN;
-    mapboxgl.accessToken = accessToken;
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9',
-        zoom: 10,
-    });
+function windCardinalDirection(degrees){
+    let cardinalDirection = '';
+    if ((degrees > 348.75 && degrees <= 360) || (degrees >=0 && degrees <= 11.25)){
+        cardinalDirection = "N";
+    } else if (degrees > 11.25 && degrees  <= 33.75) {
+        cardinalDirection = "NNE";
+    } else if (degrees > 33.75 && degrees <= 56.25) {
+        cardinalDirection = "NE";
+    } else if (degrees > 56.25 && degrees <= 78.75) {
+        cardinalDirection = "ENE";
+    } else if (degrees > 78.75 && degrees <= 101.25) {
+        cardinalDirection = "E";
+    } else if (degrees > 101.25 && degrees <= 123.75) {
+        cardinalDirection = "ESE";
+    } else if (degrees > 123.75 && degrees <= 146.25) {
+        cardinalDirection = "SE";
+    } else if (degrees > 146.25 && degrees <= 168.75) {
+        cardinalDirection = "SSE";
+    } else if (degrees > 168.75 && degrees <= 191.25) {
+        cardinalDirection = "S";
+    } else  if (degrees > 191.25 && degrees <= 213.75) {
+        cardinalDirection = "SSW";
+    } else if (degrees > 213.75 && degrees <= 236.25)  {
+        cardinalDirection = "SW";
+    } else if (degrees > 236.25 && degrees <= 258.75) {
+        cardinalDirection = "WSW";
+    } else if (degrees > 258.75 && degrees <= 281.25) {
+        cardinalDirection = "W";
+    } else if (degrees > 281.25 && degrees <= 303.75) {
+        cardinalDirection = "WNW";
+    } else if (degrees > 303.75 && degrees <= 326.25) {
+        cardinalDirection = "NW";
+    } else if (degrees > 326.75 && degrees <= 348.75) {
+        cardinalDirection = "NNW";
+    }
+    return cardinalDirection;
+}
+
 
 
