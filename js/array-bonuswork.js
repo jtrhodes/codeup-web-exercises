98964b5e180ca3a33fa222031006789d5262b90e
Jonathan <jonathan.trhodes07@gmail.com>
2022-01-07 19:49:47 -0600

    javascript practice

--- js/array-bonuswork.js
+++ js/array-bonuswork.js
@@ -304,9 +304,33 @@ function moveFirstToLast(array){
 //     – using a for loop, iterate through the array to log the shapes until the matching shape is found
 //     – once the shape is found, log a message (“Shape is found”) and use a break statement to exit loop.
 // 2.  Create a function that returns a random day of the week
+function randomDay(){
+    let day = Math.floor(Math.random() * 7 +1)
+    if(day===1){return "Monday"}
+    if(day===2){return "Tuesday"}
+    if(day===3){return "Wednesday"}
+    if(day===4){return "Thursday"}
+    if(day===5){return "Friday"}
+    if(day===6){return "Saturday"}
+    if(day===7){return "Sunday"}
+}
+
+console.log(randomDay())
 // 3.  Create a function that takes a single letter and returns what number the letter is in the alphabet. Ignore case.
 // – someFunction("a") // returns 1
 // – someFunction("z") // returns 26
+function alphabetNumber(letter){
+    let position
+    let letters = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    letters.forEach(function(element,index){
+        if(element == letter){
+            position = index
+        }
+    })
+    return position
+}
+
+console.log(alphabetNumber('u'))
 // 4.  Create a function that returns the longest string in a given array of string elements.
 // 5.   Create a function that takes in two arrays of elements, including numbers. If all numbers added together in the first array is equal to all the number inputs added in the second array, return true, otherwise, false. Only add together numeric elements but either array may contain non-numeric elements.
 //     – var arr1 = ['bob', 1, true, 1, 2];
